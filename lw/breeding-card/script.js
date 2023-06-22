ca50b7a3edfc2b670022bf82337afdb7129c6926
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2023-06-22 14:52:23 +1000

    Add glow option to LW BC

--- lw/breeding-card/script.js
+++ lw/breeding-card/script.js
@@ -55,160 +55,194 @@ function generate() {
 
 function placeWolfLayers(ctx, x) {
   // Place wolf images
-  var w1_base = new Image();
-  w1_base.onload = function () {
-    ctx.drawImage(w1_base, 0, 0, 800, 800, 0, 0, x, x);
+  var w1_glow = new Image();
+  w1_glow.onload = function () {
+    ctx.drawImage(w1_glow, 0, 0, 800, 800, 0, 0, x, x);
 
-    var w1_top = new Image();
-    w1_top.onload = function () {
-      ctx.drawImage(w1_top, 0, 0, 800, 800, 0, 0, x, x);
+    var w1_base = new Image();
+    w1_base.onload = function () {
+      ctx.drawImage(w1_base, 0, 0, 800, 800, 0, 0, x, x);
 
-      var w1_under = new Image();
-      w1_under.onload = function () {
-        ctx.drawImage(w1_under, 0, 0, 800, 800, 0, 0, x, x);
+      var w1_top = new Image();
+      w1_top.onload = function () {
+        ctx.drawImage(w1_top, 0, 0, 800, 800, 0, 0, x, x);
 
-        var w1_accent = new Image();
-        w1_accent.onload = function () {
-          ctx.drawImage(w1_accent, 0, 0, 800, 800, 0, 0, x, x);
+        var w1_under = new Image();
+        w1_under.onload = function () {
+          ctx.drawImage(w1_under, 0, 0, 800, 800, 0, 0, x, x);
 
-          var w1_eyes = new Image();
-          w1_eyes.onload = function () {
-            ctx.drawImage(w1_eyes, 0, 0, 800, 800, 0, 0, x, x);
+          var w1_accent = new Image();
+          w1_accent.onload = function () {
+            ctx.drawImage(w1_accent, 0, 0, 800, 800, 0, 0, x, x);
 
-            var w1_over = new Image();
-            w1_over.onload = function () {
-              ctx.drawImage(w1_over, 0, 0, 800, 800, 0, 0, x, x);
-              placeElementAsSVG("range-eyes", canvas, BASE_OFFSET + 200);
-              placeElementAsSVG("range-accent", canvas, BASE_OFFSET + 230);
-              placeElementAsSVG("range-under", canvas, BASE_OFFSET + 260);
-              placeElementAsSVG("range-top", canvas, BASE_OFFSET + 290);
-              placeElementAsSVG("range-base", canvas, BASE_OFFSET + 320);
-              // Write Text
-              new FontFace(
-                "FontFamily Paytone One",
-                "url(https://fonts.gstatic.com/s/paytoneone/v21/0nksC9P7MfYHj2oFtYm2ChTtgPs.woff2)",
-                {
-                  weight: 400,
-                }
-              )
-                .load()
-                .then((f) => {
-                  document.fonts.add(f);
-                  ctx.fillStyle = $("#font-color").val();
-                  ctx.font = '36px "FontFamily Paytone One"';
-                  ctx.textAlign = "center";
-                  ctx.fillText($("#name").val() || "", x, BASE_OFFSET);
-                });
+            var w1_eyes = new Image();
+            w1_eyes.onload = function () {
+              ctx.drawImage(w1_eyes, 0, 0, 800, 800, 0, 0, x, x);
+
+              var w1_over = new Image();
+              w1_over.onload = function () {
+                ctx.drawImage(w1_over, 0, 0, 800, 800, 0, 0, x, x);
+                placeElementAsSVG("range-eyes", canvas, BASE_OFFSET + 200);
+                placeElementAsSVG("range-accent", canvas, BASE_OFFSET + 230);
+                placeElementAsSVG("range-under", canvas, BASE_OFFSET + 260);
+                placeElementAsSVG("range-top", canvas, BASE_OFFSET + 290);
+                placeElementAsSVG("range-base", canvas, BASE_OFFSET + 320);
+                // Write Text
+                new FontFace(
+                  "FontFamily Paytone One",
+                  "url(https://fonts.gstatic.com/s/paytoneone/v21/0nksC9P7MfYHj2oFtYm2ChTtgPs.woff2)",
+                  {
+                    weight: 400,
+                  }
+                )
+                  .load()
+                  .then((f) => {
+                    document.fonts.add(f);
+                    ctx.fillStyle = $("#font-color").val();
+                    ctx.font = '36px "FontFamily Paytone One"';
+                    ctx.textAlign = "center";
+                    ctx.fillText($("#name").val() || "", x, BASE_OFFSET);
+                  });
+              };
+              w1_over.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+                "#w1-breed"
+              ).val()}_1_1_${$("#w1-sex").val()}_0_${SIZE}.png`;
             };
-            w1_over.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+            w1_eyes.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
               "#w1-breed"
-            ).val()}_1_1_${$("#w1-sex").val()}_0_${SIZE}.png`;
+            ).val()}_1_${$("#w1-eyes-color").val()}_${$(
+              "#w1-sex"
+            ).val()}_1_${SIZE}.png`;
           };
-          w1_eyes.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+          w1_accent.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
             "#w1-breed"
-          ).val()}_1_${$("#w1-eyes-color").val()}_${$(
+          ).val()}_${$("#w1-accent").val()}_${$("#w1-accent-color").val()}_${$(
             "#w1-sex"
-          ).val()}_1_${SIZE}.png`;
+          ).val()}_2_${SIZE}.png`;
         };
-        w1_accent.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+        w1_under.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
           "#w1-breed"
-        ).val()}_${$("#w1-accent").val()}_${$("#w1-accent-color").val()}_${$(
+        ).val()}_${$("#w1-under").val()}_${$("#w1-under-color").val()}_${$(
           "#w1-sex"
-        ).val()}_2_${SIZE}.png`;
+        ).val()}_3_${SIZE}.png`;
       };
-      w1_under.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+      w1_top.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
         "#w1-breed"
-      ).val()}_${$("#w1-under").val()}_${$("#w1-under-color").val()}_${$(
+      ).val()}_${$("#w1-top").val()}_${$("#w1-top-color").val()}_${$(
         "#w1-sex"
-      ).val()}_3_${SIZE}.png`;
+      ).val()}_4_${SIZE}.png`;
     };
-    w1_top.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+    w1_base.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
       "#w1-breed"
-    ).val()}_${$("#w1-top").val()}_${$("#w1-top-color").val()}_${$(
+    ).val()}_1_${$("#w1-base-color").val()}_${$(
       "#w1-sex"
-    ).val()}_4_${SIZE}.png`;
+    ).val()}_5_${SIZE}.png`;
   };
-  w1_base.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
-    "#w1-breed"
-  ).val()}_1_${$("#w1-base-color").val()}_${$("#w1-sex").val()}_5_${SIZE}.png`;
+  var w1_glowpath = $("#glownone").prop("checked")
+    ? "./wolfassets/Empty/EMPTY.png"
+    : $("#glowsoft").prop("checked")
+    ? `./wolfassets/Glowline/LayerImage_${$("#w1-breed").val()}_1_1_${$(
+        "#w1-sex"
+      ).val()}_0_${SIZE}_GLOW.png`
+    : `./wolfassets/Hardline/LayerImage_${$("#w1-breed").val()}_1_1_${$(
+        "#w1-sex"
+      ).val()}_0_${SIZE}_OUTLINE.png`;
+  w1_glow.src = w1_glowpath;
 
   // Place wolf images
-  var w2_base = new Image();
-  w2_base.onload = function () {
-    ctx.drawImage(w2_base, 0, 0, 800, 800, x, 0, x, x);
+  var w2_glow = new Image();
+  w2_glow.onload = function () {
+    ctx.drawImage(w2_glow, 0, 0, 800, 800, x, 0, x, x);
 
-    var w2_top = new Image();
-    w2_top.onload = function () {
-      ctx.drawImage(w2_top, 0, 0, 800, 800, x, 0, x, x);
+    var w2_base = new Image();
+    w2_base.onload = function () {
+      ctx.drawImage(w2_base, 0, 0, 800, 800, x, 0, x, x);
 
-      var w2_under = new Image();
-      w2_under.onload = function () {
-        ctx.drawImage(w2_under, 0, 0, 800, 800, x, 0, x, x);
+      var w2_top = new Image();
+      w2_top.onload = function () {
+        ctx.drawImage(w2_top, 0, 0, 800, 800, x, 0, x, x);
 
-        var w2_accent = new Image();
-        w2_accent.onload = function () {
-          ctx.drawImage(w2_accent, 0, 0, 800, 800, x, 0, x, x);
+        var w2_under = new Image();
+        w2_under.onload = function () {
+          ctx.drawImage(w2_under, 0, 0, 800, 800, x, 0, x, x);
 
-          var w2_eyes = new Image();
-          w2_eyes.onload = function () {
-            ctx.drawImage(w2_eyes, 0, 0, 800, 800, x, 0, x, x);
+          var w2_accent = new Image();
+          w2_accent.onload = function () {
+            ctx.drawImage(w2_accent, 0, 0, 800, 800, x, 0, x, x);
 
-            var w2_over = new Image();
-            w2_over.onload = function () {
-              ctx.drawImage(w2_over, 0, 0, 800, 800, x, 0, x, x);
-              placeElementAsSVG("range-eyes", canvas, BASE_OFFSET + 200);
-              placeElementAsSVG("range-accent", canvas, BASE_OFFSET + 230);
-              placeElementAsSVG("range-under", canvas, BASE_OFFSET + 260);
-              placeElementAsSVG("range-top", canvas, BASE_OFFSET + 290);
-              placeElementAsSVG("range-base", canvas, BASE_OFFSET + 320);
-              // Write Text
-              new FontFace(
-                "FontFamily Paytone One",
-                "url(https://fonts.gstatic.com/s/paytoneone/v21/0nksC9P7MfYHj2oFtYm2ChTtgPs.woff2)",
-                {
-                  weight: 400,
-                }
-              )
-                .load()
-                .then((f) => {
-                  document.fonts.add(f);
-                  ctx.fillStyle = $("#font-color").val();
-                  ctx.font = '36px "FontFamily Paytone One"';
-                  ctx.textAlign = "center";
-                  ctx.fillText($("#name").val() || "", x, BASE_OFFSET);
-                });
+            var w2_eyes = new Image();
+            w2_eyes.onload = function () {
+              ctx.drawImage(w2_eyes, 0, 0, 800, 800, x, 0, x, x);
+
+              var w2_over = new Image();
+              w2_over.onload = function () {
+                ctx.drawImage(w2_over, 0, 0, 800, 800, x, 0, x, x);
+                placeElementAsSVG("range-eyes", canvas, BASE_OFFSET + 200);
+                placeElementAsSVG("range-accent", canvas, BASE_OFFSET + 230);
+                placeElementAsSVG("range-under", canvas, BASE_OFFSET + 260);
+                placeElementAsSVG("range-top", canvas, BASE_OFFSET + 290);
+                placeElementAsSVG("range-base", canvas, BASE_OFFSET + 320);
+                // Write Text
+                new FontFace(
+                  "FontFamily Paytone One",
+                  "url(https://fonts.gstatic.com/s/paytoneone/v21/0nksC9P7MfYHj2oFtYm2ChTtgPs.woff2)",
+                  {
+                    weight: 400,
+                  }
+                )
+                  .load()
+                  .then((f) => {
+                    document.fonts.add(f);
+                    ctx.fillStyle = $("#font-color").val();
+                    ctx.font = '36px "FontFamily Paytone One"';
+                    ctx.textAlign = "center";
+                    ctx.fillText($("#name").val() || "", x, BASE_OFFSET);
+                  });
+              };
+              w2_over.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+                "#w2-breed"
+              ).val()}_1_1_${$("#w2-sex").val()}_0_${SIZE}.png`;
             };
-            w2_over.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+            w2_eyes.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
               "#w2-breed"
-            ).val()}_1_1_${$("#w2-sex").val()}_0_${SIZE}.png`;
+            ).val()}_1_${$("#w2-eyes-color").val()}_${$(
+              "#w2-sex"
+            ).val()}_1_${SIZE}.png`;
           };
-          w2_eyes.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+          w2_accent.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
             "#w2-breed"
-          ).val()}_1_${$("#w2-eyes-color").val()}_${$(
+          ).val()}_${$("#w2-accent").val()}_${$("#w2-accent-color").val()}_${$(
             "#w2-sex"
-          ).val()}_1_${SIZE}.png`;
+          ).val()}_2_${SIZE}.png`;
         };
-        w2_accent.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+        w2_under.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
           "#w2-breed"
-        ).val()}_${$("#w2-accent").val()}_${$("#w2-accent-color").val()}_${$(
+        ).val()}_${$("#w2-under").val()}_${$("#w2-under-color").val()}_${$(
           "#w2-sex"
-        ).val()}_2_${SIZE}.png`;
+        ).val()}_3_${SIZE}.png`;
       };
-      w2_under.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+      w2_top.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
         "#w2-breed"
-      ).val()}_${$("#w2-under").val()}_${$("#w2-under-color").val()}_${$(
+      ).val()}_${$("#w2-top").val()}_${$("#w2-top-color").val()}_${$(
         "#w2-sex"
-      ).val()}_3_${SIZE}.png`;
+      ).val()}_4_${SIZE}.png`;
     };
-    w2_top.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
+    w2_base.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
       "#w2-breed"
-    ).val()}_${$("#w2-top").val()}_${$("#w2-top-color").val()}_${$(
+    ).val()}_1_${$("#w2-base-color").val()}_${$(
       "#w2-sex"
-    ).val()}_4_${SIZE}.png`;
+    ).val()}_5_${SIZE}.png`;
   };
-  w2_base.src = `https://lorwolf.azureedge.net/rawwolflayers/LayerImage_${$(
-    "#w2-breed"
-  ).val()}_1_${$("#w2-base-color").val()}_${$("#w2-sex").val()}_5_${SIZE}.png`;
+  var w2_glowpath = $("#glownone").prop("checked")
+    ? "./wolfassets/Empty/EMPTY.png"
+    : $("#glowsoft").prop("checked")
+    ? `./wolfassets/Glowline/LayerImage_${$("#w2-breed").val()}_1_1_${$(
+        "#w2-sex"
+      ).val()}_0_${SIZE}_GLOW.png`
+    : `./wolfassets/Hardline/LayerImage_${$("#w2-breed").val()}_1_1_${$(
+        "#w2-sex"
+      ).val()}_0_${SIZE}_OUTLINE.png`;
+  w2_glow.src = w2_glowpath;
 }
 
 function placeElementAsSVG(id, canvas, y) {
