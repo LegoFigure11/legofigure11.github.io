a0986df6e28ea5e4389ef75fb933de3df9fd768d
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2025-10-26 17:23:35 +1100

    Add PS! Color tools

---
+++ smogon/ps/color/search.js
@@ -0,0 +1,35 @@
+const CHARSET = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+function shuffleArray(array) {
+  const shuffledArray = [...array]; 
+
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); 
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  return shuffledArray.join("");
+}
+
+function indexToString(index, charset, length) {
+  const chars = new Array(length);
+  const baseN = CHARSET.length;
+  let v = Number(index); // ensure number
+  for (let i = 0; i < length; i++) {
+    chars[i] = charset[v % baseN];
+    v = Math.floor(v / baseN);
+  }
+
+  return chars.join('');
+}
+
+function search2(start, length, target) {
+    const pow = Math.pow(36, length);
+    let found = false;
+    const chars = shuffleArray(CHARSET);
+    for (let i = start + 1; i != start && !found; i = (i + 1) % pow) {
+        const name = indexToString(i, chars, length);
+        const color = usernameColor(name);
+        if (color === target) return name;
+    }
+}
